e67704270780baa7e1dc479018b5a183c65b1171
daehyun <kimdeahyeon5445@gmail.com>
2025-04-15 20:51:08 +0900

    [feat]: Add dummy header

--- script/loader.js
+++ script/loader.js
@@ -6,10 +6,43 @@
 // header로드하는 함수입니다.
 const makeHeader = () => {
     const header = document.getElementById("header");
-    const path = window.location.pathname.split("/").pop();
-    header.innerHTML = `
-        <h1>LOGO</h1>
-        `;
+    // 현재 페이지의 경로를 가져옵니다.
+    const path = window.location.pathname;
+    console.log(path);
+    // logo를 생성합니다.
+    const logo = document.createElement("div");
+    header.appendChild(logo);
+
+    // navbar를 생성합니다.
+    const menu = document.createElement("div");
+    menu.classList.add("menu");
+    const nav = document.createElement("nav");
+    nav.classList.add("nav_container");
+    const ul = document.createElement("ul");
+    ul.classList.add("nav_wrapper");
+    const navItems = [
+        { name: "홈", path: "/index.html" },
+        { name: "테마", path: "#" },
+        { name: "지역", path: "/pages/regions.html" },
+        { name: "여행코스", path: "#" },
+        { name: "여행정보", path: "#" },
+        { name: "여행혜택", path: "#" },
+    ];
+    for (let i = 0; i < navItems.length; i++) {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.href = navItems[i].path;
+        a.innerText = navItems[i].name;
+        if (path === navItems[i].path) {
+            a.classList.add("active");
+        }
+        li.appendChild(a);
+        ul.appendChild(li);
+    }
+    nav.appendChild(ul);
+    menu.appendChild(nav);
+    header.appendChild(menu);
+
     return;
 };
 
