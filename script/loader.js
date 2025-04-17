32b8a3166e481c2c75a772802f24ccea25d26fb1
daehyun <kimdeahyeon5445@gmail.com>
2025-04-17 11:34:22 +0900

    [fix] link pages

--- script/loader.js
+++ script/loader.js
@@ -1,11 +1,11 @@
 // header, footer 자동 로드
 document.addEventListener("DOMContentLoaded", () => {
     const loadComponent = async (selector, file) => {
-      const res = await fetch(file);
-      const html = await res.text();
-      document.querySelector(selector).innerHTML = html;
+        const res = await fetch(file);
+        const html = await res.text();
+        document.querySelector(selector).innerHTML = html;
     };
-  
-    loadComponent("#header", "header.html");
-    loadComponent("#footer", "footer.html");
-  });
+
+    loadComponent("#header", "/header.html");
+    loadComponent("#footer", "/footer.html");
+});
