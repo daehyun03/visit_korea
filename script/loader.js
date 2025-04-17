6c4c66e190fa0fe982b337a67900200ec8962825
daehyun <kimdeahyeon5445@gmail.com>
2025-04-17 15:12:33 +0900

    [feat]: highlight selcted tag and fix header

--- script/loader.js
+++ script/loader.js
@@ -1,11 +1,29 @@
 // header, footer 자동 로드
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
     const loadComponent = async (selector, file) => {
         const res = await fetch(file);
         const html = await res.text();
         document.querySelector(selector).innerHTML = html;
     };
 
-    loadComponent("#header", "/header.html");
-    loadComponent("#footer", "/footer.html");
+    await loadComponent("#header", "/header.html");
+    await loadComponent("#footer", "/footer.html");
+    updateSelectedNav();
 });
+
+const updateSelectedNav = () => {
+    const currentPath = window.location.pathname;
+    const navLinks = document.getElementsByClassName("header_tag");
+    Array.from(navLinks).forEach((link) => {
+        if (link.getAttribute("href") === currentPath) {
+            link.classList.add("selected");
+        } else if (
+            "/pages/event.html" === currentPath &&
+            link.innerHTML === "여행혜택"
+        ) {
+            link.classList.add("selected");
+        } else {
+            link.classList.remove("selected");
+        }
+    });
+};
